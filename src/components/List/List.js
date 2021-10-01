1e0b988f81bc2278482345a7c7c72af51ccff0b9
Adrian Sadowski <sadow.adrian@gmail.com>
2021-10-01 22:24:28 +0200

    add child props list

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -2,12 +2,16 @@ import React from 'react';
 import styles from './List.scss';
 import Hero from '../Hero/Hero';
 
+//next step Sprawdzanie typów wartości w propsach
 
 class List extends React.Component {
   render() {
     return (
       <section className={styles.component}>
         <Hero titleText={this.props.title} />
+        <div className={styles.description}>
+          {this.props.children}
+        </div>
       </section>
     )
   }
