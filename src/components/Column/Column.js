86f4b26702ad5614605dff8af399adf974302f57
Adrian Sadowski <sadow.adrian@gmail.com>
2021-10-09 16:01:39 +0200

    add function add cards redux

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -2,14 +2,14 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import { settings } from '../../data/dataStore';
 import Card from '../Card/Card';
-// import Creator from '../Creator/Creator';
+import Creator from '../Creator/Creator';
 import Icon from '../Icon/Icon';
 import styles from './Column.scss';
 
-
+// zaginione cards ???????
 class Column extends React.Component {
   render() {
-    const {title, icon, cards} = this.props;
+    const {title, icon, cards, addCard} = this.props;
     
     return (
       <section className={styles.component}>
@@ -22,16 +22,17 @@ class Column extends React.Component {
             <Card key ={cardData.id} {...cardData} />
           ))}
         </div>
-        {/* <div className={styles.creator}>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)}/>
-        </div> */}
+        <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={addCard}/>
+        </div>
       </section>
     );
   }
   static propTypes = {
-    title: PropTypes.node,
-    icon: PropTypes.string,
+    title: PropTypes.string,
+    icon: PropTypes.node,
     cards: PropTypes.array,
+    addCard: PropTypes.func,
   };
   static defaultProps = {
     icon: settings.defaultColumnIcon,
