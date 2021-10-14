3cf330b9a9dd40c3d68f4899535799c5d15ed2c5
Adrian Sadowski <sadow.adrian@gmail.com>
2021-10-14 13:36:33 +0200

    Add React route, add MainLaylout

---
+++ src/components/Home/Home.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import List from '../List/ListContainer';
+import styles from './Home.scss';
+import PropTypes from 'prop-types';
+import Search from '../Search/SearchContainer';
+
+
+class Home extends React.Component {
+  render() {
+    const { title, subtitle, lists} = this.props;
+
+    return (
+      <main className={styles.component}>
+        <h1 className={styles.title}>{title}</h1>
+        <h2 className={styles.subtitle}>{subtitle}</h2>
+        <Search />
+        {lists.map(listData => (
+          <List key={listData.id} {...listData} />
+        ))}
+      </main>
+    );
+  }
+  static propTypes = {
+    title: PropTypes.node,
+    subtitle: PropTypes.node,
+    lists: PropTypes.array,
+    searchString: PropTypes.string,
+  }
+}
+
+export default Home;
